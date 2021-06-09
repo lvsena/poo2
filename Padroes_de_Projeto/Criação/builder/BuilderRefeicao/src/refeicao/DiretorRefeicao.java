34afa3dfa70d30afce6517a1ae9c57d912f6e7e5
Leonardo Sena <leosena1995@gmail.com>
2021-06-08 22:15:27 -0300

    feat: exercicio refeição lanchonete

--- Padroes_de_Projeto/Criação/builder/BuilderRefeicao/src/refeicao/DiretorRefeicao.java
+++ Padroes_de_Projeto/Criação/builder/BuilderRefeicao/src/refeicao/DiretorRefeicao.java
@@ -1,31 +1,45 @@
-
 package refeicao;
 
 /**
  *
  * @author felipe
  */
-public class DiretorRefeicao {        
-    
-    public Refeicao preparaNaoVegetariana(String sabor, String tipoBebida){       
+public class DiretorRefeicao {
+
+    public Refeicao preparaNaoVegetariana(String sabor, String tipoBebida, Boolean comBatata) {
         Refeicao novaRefeicao = new Refeicao();
-        NonVegetarianoBuilder refeicaoBuilder = new NonVegetarianoBuilder(novaRefeicao);        
-        if(tipoBebida.equalsIgnoreCase("refrigerante"))
+        NonVegetarianoBuilder refeicaoBuilder = new NonVegetarianoBuilder(novaRefeicao);
+
+        if (tipoBebida.equalsIgnoreCase("refrigerante")) {
             refeicaoBuilder.adicionarRefrigerante(sabor);
-        else
-            refeicaoBuilder.adicionarSuco(sabor);                
+        } else {
+            refeicaoBuilder.adicionarSuco(sabor);
+        }
+
+        if (comBatata) {
+            refeicaoBuilder.preparaBatata();
+        }
+
         refeicaoBuilder.prepadaHamburgerCarne();
-        return novaRefeicao;        
-    }     
-    public Refeicao preparaVegetariana(String sabor, String tipoBebida){        
+        return novaRefeicao;
+    }
+
+    public Refeicao preparaVegetariana(String sabor, String tipoBebida, Boolean comBatata) {
         Refeicao novaRefeicao = new Refeicao();
-        VegetarianoBuilder refeicaoBuilder = new VegetarianoBuilder(novaRefeicao);        
-        if(tipoBebida.equalsIgnoreCase("refrigerante"))
+        VegetarianoBuilder refeicaoBuilder = new VegetarianoBuilder(novaRefeicao);
+
+        if (tipoBebida.equalsIgnoreCase("refrigerante")) {
             refeicaoBuilder.adicionarRefrigerante(sabor);
-        else
-            refeicaoBuilder.adicionarSuco(sabor);     
+        } else {
+            refeicaoBuilder.adicionarSuco(sabor);
+        }
+
+        if (comBatata) {
+            refeicaoBuilder.preparaBatata();
+        }
+
         refeicaoBuilder.preparaHamburgerVegetariano();
-        return novaRefeicao;        
-    }            
-    
+        return novaRefeicao;
+    }
+
 }
--- Padroes_de_Projeto/Criação/builder/BuilderRefeicao/src/refeicao/RefeicaoBuilder.java
+++ Padroes_de_Projeto/Criação/builder/BuilderRefeicao/src/refeicao/RefeicaoBuilder.java
@@ -1,20 +1,22 @@
-
-
 package refeicao;
 
 /**
  *
  * @author felipe
  */
-public abstract class RefeicaoBuilder {    
-    Refeicao refeicao;        
-    
-    public void adicionarSuco(String sabor){                                
-        refeicao.setBebida(new Suco(sabor));        
-    }   
-       
-    public void adicionarRefrigerante(String sabor){
-        refeicao.setBebida(new Refrigerante(sabor));        
-    }   
-    
+public abstract class RefeicaoBuilder {
+
+    Refeicao refeicao;
+
+    public void adicionarSuco(String sabor) {
+        refeicao.setBebida(new Suco(sabor));
+    }
+
+    public void adicionarRefrigerante(String sabor) {
+        refeicao.setBebida(new Refrigerante(sabor));
+    }
+
+    public void preparaBatata() {
+        refeicao.setBatata(new Batata());
+    }
 }
